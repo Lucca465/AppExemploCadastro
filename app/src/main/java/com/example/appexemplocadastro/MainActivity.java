59fc37d3435283565103ba2bba956eebf944c100
Lucca465 <lucca465@gmail.com>
2021-11-02 23:15:44 -0300

    configuração no firebase e interface criado

--- app/src/main/java/com/example/appexemplocadastro/MainActivity.java
+++ app/src/main/java/com/example/appexemplocadastro/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.appexemplocadastro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void btEntrar(View view){
+        startActivity(new Intent(this, TelaLogin.class));
+    }
+
+    public void btCadastrar(View view){
+        startActivity(new Intent(this, TelaCadastro.class));
+    }
+
 }
